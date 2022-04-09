ce4fd408e07ff38b7cb2e36d4ca8d1c019f4bb6f
nani199823 <53505468+nani199823@users.noreply.github.com>
2022-04-08 21:43:46 -0500

    Update Sdata.js

--- src/Sdata.js
+++ src/Sdata.js
@@ -1,9 +1,3 @@
-import c from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/1.jpeg";
-import java from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/2.jpeg";
-import php from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/3.jpeg";
-import python from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/4.jpeg";
-import andriod from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/5.jpeg";
-import web from "/Users/anikethvyas/Downloads/multipage-react-website-master/src/images/6.jpeg";
 
 const Sdata = [
     {
